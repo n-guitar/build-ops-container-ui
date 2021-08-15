2b20202a6d757602325dbd72e7d6e94aac934403
n-guitar <nguitar001@gmail.com>
2021-08-15 21:12:04 +0900

    eslintの設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,9 +68,16 @@ export default {
    ** Build configuration
    */
   build: {
-    /*
-     ** You can extend webpack config here
-     */
-    extend(config, ctx) {}
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
   }
 }
