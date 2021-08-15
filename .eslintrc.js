5d4059ba8efa67fce4c93b53c2cdc8d4a06d312e
n-guitar <nguitar001@gmail.com>
2021-08-15 21:11:30 +0900

    rulesの変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,15 @@ module.exports = {
   plugins: ['prettier'],
   // add your custom rules here
   rules: {
-    'vue/comment-directive': 0
+    'vue/comment-directive': 0,
+    'no-console': 'off', //console.log();OK
+    'no-unused-vars': 'off', //使っていない変数あってもOK
+    'vue/html-self-closing': 'off', //imgタグのようにタグが１つで完結してもOK
+    'prettier/prettier': [
+      'error',
+      {
+        htmlWhitespaceSensitivity: 'ignore' //aタグの羅列などで変な風にならないように追記
+      }
+    ]
   }
 }
